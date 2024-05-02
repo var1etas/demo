d89b69aa9621e894510c3e5f87635dd6bc09e0cd
Alex <filatovalexandr2003@gmail.com>
2024-05-02 17:51:25 +0500

    fix property injection

--- src/main/java/com/example/factory/config/APIConfig.java
+++ src/main/java/com/example/factory/config/APIConfig.java
@@ -1,12 +1,14 @@
 package com.example.factory.config;
 
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Getter
+@Setter
+@ConfigurationProperties(prefix = "api")
 public class APIConfig {
-    @Value("${api.limit}")
     int limit;
 }
